4318cff34e8a76e72bc0a069947789c1fad939ad
David Gross <dgroseph@gmail.com>
2016-01-20 23:10:43 -0800

    Started work on solution

---
+++ PuzzleMat/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace PuzzleMat
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("PuzzleMat")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("davidgross")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
