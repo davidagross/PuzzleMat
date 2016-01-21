840d5fe934349c87ebca8d708c7b8e22b28f549d
David Gross <dgroseph@gmail.com>
2016-01-20 23:29:50 -0800

    Added some preliminary types

---
+++ PuzzleMat/PuzzleMat.fs
@@ -0,0 +1,13 @@
+﻿namespace PuzzleMat
+
+module PuzzleMatTypes = 
+
+    type Row = Row of string
+    type Column = Column of string
+
+    type Mat = 
+        {
+            Letters:string ; 
+            Rows:int ; 
+            Columns: int
+        }
